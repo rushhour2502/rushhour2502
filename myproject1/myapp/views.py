c5d00502904c175fd4ac569680baa81311da39f9
rushhour2502 <azekhuoriadignity@gmail.com>
2025-10-16 21:43:32 +0100

    test

--- myproject1/myapp/views.py
+++ myproject1/myapp/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello bro 👋🏽, welcome to my first Django app!")
+    return HttpResponse("Hello bro 👋🏽, welcome to my first Django app! h")
 
 # Create your views here.
